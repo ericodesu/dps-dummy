d95df1313612fffde91dac21cb9e07eb673d7cc4
ericodess <erickfcaldeira@gmail.com>
2022-05-15 16:24:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DpsDummy
+
+Developed with Unreal Engine 5
---
+++ Source/DpsDummy/DpsDummyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DpsDummyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DPSDUMMY_API ADpsDummyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
